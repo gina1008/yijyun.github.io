7b216e30395550b46b0681046de4db1fe5ab7cef
gina1008 <55227311+gina1008@users.noreply.github.com>
2019-11-27 05:01:12 +0800

    123

--- js/hw2.js
+++
@@ -1,6 +0,0 @@
-function sethere()
-{
-    $('.here').css({'opacity':0,});
-	$('#'+sethere.arguments[0]+' .here').css({'opacity':1,});
-	$('html,body').animate( { scrollTop: ($('#'+sethere.arguments[0]).offset().top)-300 }, 800);
-}
